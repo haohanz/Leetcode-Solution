08376c815bb02e28f0ee0878af2411bc4cf0a1bb
haohanz <hh1680651@gmail.com>
2019-08-02 01:41:06 -0400

    update 290_word_pattern.py

--- 290_word_pattern.py
+++ 290_word_pattern.py
@@ -10,3 +10,14 @@ class Solution(object):
         # get index list of first occurance)
         return map(pattern.find, pattern) == map(str.index, str)
 
+    def wordPattern(self, pattern, str):
+        """
+        :type pattern: str
+        :type str: str
+        :rtype: bool
+        """
+        str = str.split()
+        f = lambda x: map({}.setdefault, x, range(len(x)))
+        # get index list of first occurance)
+        return f(pattern) == f(str)
+
